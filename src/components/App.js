c3a736f7c9b55252a337221d1b66357c1d69fcde
Ryan Doherty <rjd.atx@gmail.com>
2020-03-29 23:16:50 -0500

    added and formatted VideoDetail, completeted formatting for VideoItem and VideoList components

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 const KEY = 'AIzaSyCCHr3cyKtDYlYNrLd-iwFCFMtblQfs6G0';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('Live Music Festival');
+    }
     
     onTermSubmit = async term => {
         const response = await youtube.get("/search", {
             params: {
                 q: term,
-                part: "snippet",
+                type: 'video',
+                part: 'snippet',
                 maxResults: 5,
                 key: KEY
             }
         });
 
-        this.setState({ videos: response.data.items });
-    }
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
     
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect} 
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
